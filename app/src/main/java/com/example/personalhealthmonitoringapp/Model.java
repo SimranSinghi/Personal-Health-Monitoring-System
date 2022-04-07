7d7529d00f5cd8ce38426129a7060119c219e2b3
SUJEETH <61650575+Sujeeth2104@users.noreply.github.com>
2022-04-07 13:52:36 -0500

    Added Diet and Recipe screen

---
+++ app/src/main/java/com/example/personalhealthmonitoringapp/Model.java
@@ -0,0 +1,37 @@
+package com.example.personalhealthmonitoringapp;
+
+public class Model {
+    String Dname,BP,HRate,ResRate,Date,Cholesterol,BodyTemp,Notes;
+
+    public String getDname() {
+        return Dname;
+    }
+
+    public String getBP() {
+        return BP;
+    }
+
+    public String getHRate() {
+        return HRate;
+    }
+
+    public String getResRate() {
+        return ResRate;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public String getCholesterol() {
+        return Cholesterol;
+    }
+
+    public String getBodyTemp() {
+        return BodyTemp;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+}
